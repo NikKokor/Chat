630f23e649be3b9303a42bc096786e4e7f011525
NikKokor <62161082+NikKokor@users.noreply.github.com>
2021-10-17 13:23:06 +1000

    Add files via upload

--- chat.php
+++ chat.php
@@ -5,41 +5,74 @@
 </head>
 <body>
 <?php
+$mode = $_GET['mode'];
 
-$login = $_GET['login'];
-$password = $_GET['password'];
-$text = $_GET['text'];
+if ($mode == 1) {              //Регистрация
+    $login = $_GET['login'];
+    $password = $_GET['password'];
+    $repeat_password = $_GET['repeat_password'];
 
-$data = json_decode(file_get_contents('Data.json'));
-$num_user = 0;
+    $data = json_decode(file_get_contents('Data.json'));
+    $user = false;
 
-for ($i = 0; $i < sizeof($data->usersData); $i++) {
-    if($data->usersData[$i]->login === $login) {
-        $num_user = $i;
-        break;
+    for ($i = 0; $i < sizeof($data->usersData); $i++) {
+        if($data->usersData[$i]->login === $login) {
+            $user = true;
+            break;
+        }
     }
-}
-
-if(($data->usersData[$num_user]->login === $login) && ($data->usersData[$num_user]->password === $password))
-    if ($text != null) {
-        $date = date('Y-m-d H:i:s');
-        $messenger = array('login' => $login, 'mess' => $text, 'date' => $date);
-        array_push($data->messages, $messenger);
+    if (($login == null or $login == "") && ($password == null or $password == ""))
+        echo "Введите логин и пароль" . "<br>";
+    else if ($user === true)
+        echo "Логин уже занят" . "<br>";
+    else if ($password !== $repeat_password)
+        echo "Пароли не совпадают" . "<br>";
+    else {
+        $account = array('login' => $login, 'password' => $password);
+        array_push($data->usersData, $account);
         file_put_contents('Data.json', json_encode($data));
-    } else {
-        echo "Нет текста сообщения"  . "<br>";
+        echo "Аккаунт создан" . "<br>";
     }
-else {
-    echo "Неверный пароль или логин"  . "<br>";
 }
+else if ($mode == 2) {         //Отправка сообщения
+    $login = $_GET['login'];
+    $password = $_GET['password'];
+    $text = $_GET['text'];
 
-$data = json_decode(file_get_contents("Data.json"));
+    $data = json_decode(file_get_contents('Data.json'));
+    $num_user = 0;
 
-for ($i = 0; $i < sizeof($data->messages); $i++) {
-    echo "----------------------------" . "<br>";
-    echo $data->messages[$i]->login . "<br>";
-    echo $data->messages[$i]->mess . "<br>";
-    echo $data->messages[$i]->date . "<br>";
+    for ($i = 0; $i < sizeof($data->usersData); $i++) {
+        if($data->usersData[$i]->login === $login) {
+            $num_user = $i;
+            break;
+        }
+    }
+
+    if(($data->usersData[$num_user]->login === $login) && ($data->usersData[$num_user]->password === $password))
+        if ($text != null) {
+            $date = date('Y-m-d H:i:s');
+            $messenger = array('login' => $login, 'mess' => $text, 'date' => $date);
+            array_push($data->messages, $messenger);
+            file_put_contents('Data.json', json_encode($data));
+        } else {
+            echo "Нет текста сообщения"  . "<br>";
+        }
+    else {
+        echo "Неверный пароль или логин"  . "<br>";
+    }
+
+    $data = json_decode(file_get_contents("Data.json"));
+
+    for ($i = 0; $i < sizeof($data->messages); $i++) {
+        echo "----------------------------" . "<br>";
+        echo $data->messages[$i]->login . "<br>";
+        echo $data->messages[$i]->mess . "<br>";
+        echo $data->messages[$i]->date . "<br>";
+    }
+}
+else {
+    echo "Выберите режим" . "<br>";
 }
 ?>
 </body>
